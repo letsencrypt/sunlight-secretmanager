224c220a79eb56e640581ee784c81fc674f70d9d
kruti-s <ksutaria@letsencrypt.org>
2024-11-21 17:02:05 -0500

    lint

--- config/load_config_test.go
+++ config/load_config_test.go
@@ -1,15 +1,17 @@
-package config
+package config_test
 
 import (
 	"reflect"
 	"testing"
+
+	"github.com/letsencrypt/sunlight-secretmanager/config"
 )
 
 func TestLoadConfigNoFile(t *testing.T) {
 	t.Parallel()
 
 	testFile := ""
-	got, err := LoadConfigFromYaml(testFile)
+	got, err := config.LoadConfigFromYaml(testFile)
 
 	if got != nil || err == nil {
 		t.Errorf("got %q and error %q, wanted error and nil error", got, err)
@@ -20,7 +22,7 @@ func TestLoadConfigCorrect(t *testing.T) {
 	t.Parallel()
 
 	testFile := "sunlight.yaml"
-	got, err := LoadConfigFromYaml(testFile)
+	got, err := config.LoadConfigFromYaml(testFile)
 	want := map[string]string{
 		"rome.ct.filippo.io/2025h1": "radiantlog-twig.ct.letsencrypt.org-2025h1b.key",
 		"rome.ct.filippo.io/2025h2": "radiantlog-twig.ct.letsencrypt.org-2025h2b.key",
